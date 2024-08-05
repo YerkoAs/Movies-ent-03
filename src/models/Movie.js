0a5afbf1cdcb1fc861591769bacba6e800a9a1fe
Yerko Asbun <asbunyerko01@gmail.com>
2024-08-05 17:56:58 -0400

    advances

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -15,7 +15,7 @@ const Movie = sequelize.define('movie', {
         allowNull: false
     },
     releaseYear: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false
     },
 });
